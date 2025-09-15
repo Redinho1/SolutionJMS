ac74887defac445fb81039bdf0fe6e0e62d31518
Rafael <redpt3219@gmail.com>
2025-09-15 11:10:51 +0200

    progrès classe PieceAgreee

--- ClassJMS/PieceAgreee.cs
+++ ClassJMS/PieceAgreee.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace ClassJMS
 {
-    class PieceAgreee
+    public class PieceAgreee : Piece
     {
+        #region Attributs privées
+        private DateTime dateAgrement;
+        private string nomConstructeur;
+        #endregion
+
+        #region Constructeur
+        public PieceAgreee(int unNumero, string unLibelle, int unNombre, DateTime uneDate, string unConstructeur)
+            : base(unNumero,unLibelle,unNombre)
+        {
+            this.dateAgrement = uneDate;
+            this.nomConstructeur = unConstructeur;
+        }
+        #endregion
+
+        #region Méthodes
+        public int CalculerDureeAgrement()
+        {
+            return 0;
+        }
+
+        public void RenouvelerAgrement(DateTime uneDate)
+        {
+            this.dateAgrement = uneDate;
+        }
+
+        public string ObtenirInfos()
+        {
+            return this.nomConstructeur;
+        }
+        #endregion
     }
 }
