a529910d049bb66d017e483dfd74a31edb50b0df
Rafael <redpt3219@gmail.com>
2025-09-15 12:01:07 +0200

    TestPNO complet

---
+++ ClassJMS/PieceNonAgreee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassJMS
+{
+    public class PieceNonAgreee : Piece
+    {
+        #region Attributs Privées
+        private string etat;
+        private int seuil;
+        #endregion
+
+        #region Constructeur
+        public PieceNonAgreee(int unNumero, string unLibelle, int unNombre, int unSeuil)
+    :       base(unNumero, unLibelle, unNombre)
+        {
+            this.seuil = unSeuil;
+            this.etat = "VERT"; //associer vers VERT, ORANGE ou ROUGE
+        }
+        #endregion
+
+        #region Méthodes 
+        public string GetEtat()
+        {
+            return this.etat;
+        }
+
+        public void ChangerEtat(string unEtat)
+        {
+            if (unEtat == "VERT" || unEtat == "ORANGE" || unEtat == "ROUGE")
+                this.etat = unEtat;
+            else
+                this.etat = "VERT";
+        }
+
+        public override string ObtenirInfos()
+        {
+            return $"{base.ObtenirInfos()}\nEtat : {GetEtat()}";
+        }
+
+        public override bool AControler()
+        {
+            if(GetEtat() == "VERT")
+            {
+                return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+}
--- ClassJMSTests/PieceNonAgreeeTests.cs
+++ ClassJMSTests/PieceNonAgreeeTests.cs
@@ -7,32 +7,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassJMS.Tests
-{ /*
+{ 
     [TestClass()]
     public class PieceNonAgreeeTests
     {
         [TestMethod()]
         public void ObtenirInfosTest()
         {
+            PieceNonAgreee PNA = new PieceNonAgreee(125, "Anémomètre", 1250,60);
+            string expected = "125 - Anémomètre\nEtat : VERT";
+            Assert.AreEqual(expected, PNA.ObtenirInfos());
         }
 
         [TestMethod()]
         public void ChangerEtatTest()
         {
+            PieceNonAgreee PNA = new PieceNonAgreee(125, "Anémomètre", 1250, 60);
+            Assert.AreEqual("VERT",PNA.GetEtat());
+            PNA.ChangerEtat("ORANGE");
+            Assert.AreEqual("ORANGE", PNA.GetEtat());
         }
 
         [TestMethod()]
         public void AControlerTest()
         {
             // Cas 1 : La piece est à contrôler car son état est vert et le seuil est dépassé
-            
+            PieceNonAgreee PNA = new PieceNonAgreee(125, "Anémomètre", 1250, 60);
+            Assert.AreEqual(false, PNA.AControler());
 
             // Cas 2 : La piece n'est pas à contrôler car le seuil n'est pas dépassé, même si son état est vert
-            
+            PNA.ChangerEtat("ORANGE");
+            Assert.AreEqual(true, PNA.AControler());
+
 
             // Cas 3 : La piece n'est pas à contrôler car son état n'est pas vert, même si le seuil est dépassé
-            
-            
+            PNA.ChangerEtat("ORANGE");
+            Assert.AreEqual(true, PNA.AControler());
+
+
         }
-    } */
+    } 
 }
